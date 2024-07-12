8d6d58b407976e0d8ba7c87815c4eb3307d435b1
Dennis Terhorst <d.terhorst@fz-juelich.de>
2024-07-12 14:56:45 +0200

    update project infrastructure

--- gathermetadata/__init__.py
+++ gathermetadata/__init__.py
@@ -1,5 +1,9 @@
-'''
-Analyse contributions to a git project.
+"""
+Collector for generic metadata of HPC execution environment focussed on
+benchmarking.
 
-Add a longer description here
-'''
+Add a longer description here.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Dennis Terhorst <d.terhorst@fz-juelich.de>"
